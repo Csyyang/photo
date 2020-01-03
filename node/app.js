43c3d501381fb6717e19c0eaa9e0a97e2fca797a
csy <693765678@qq.com>
2020-01-03 16:21:37 +0800

    node

---
+++ node/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+app.use('/',express.static('../dist'));
+
+app.get('/',function(req,res) {
+    res.sendFile(path.resolve(__dirname,"../dist/index.html"));
+})
+
+app.listen(80,function() {
+    console.log('80端口')
+});
