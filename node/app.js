a113a86400c946e2c2d8d79cd18ecb1f4bab642c
csy <693765678@qq.com>
2020-01-03 17:00:07 +0800

    update

--- node/app.js
+++ node/app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express();
 const path = require('path')
 
-app.use('/',express.static('../dist'));
+app.use('/',express.static('./dist'));
 
 app.get('/',function(req,res) {
-    res.sendFile(path.resolve(__dirname,"../dist/index.html"));
+    res.sendFile(path.resolve(__dirname,"./dist/index.html"));
 })
 
 app.listen(80,function() {
